7c83ab0eb3711b29a8ea2d6dadc509f1c1f7eaa7
spencerahill <spencerahill@gmail.com>
2015-08-12 16:30:04 -0400

    Remove redundant PlotParams class from plot.py

--- aospy_user/plot.py
+++ aospy_user/plot.py
@@ -14,21 +14,21 @@ from aospy_user import regions as reg
 def plot(plot_params):
     fig = aospy.plotting.Fig(
         plot_params,
-        n_row=2,
-        n_col=2,
+        n_row=1,
+        n_col=1,
         n_ax='all',
         n_plot=1,
         n_data=1,
 
-        row_size=4,
+        row_size=3,
         col_size=3,
         subplot_lims={'left': 0.05, 'right': 0.95, 'wspace': 0.1,
                       'bottom': 0.1, 'top': 0.95, 'hspace': 0.1},
 
-        min_cntr=200,
-        max_cntr=300,
-        num_cntr=20,
-        contourf_extend='min',  # 'auto' 'neither' 'min' 'max' 'both'
+        min_cntr=-37.5,
+        max_cntr=37.5,
+        num_cntr=15,
+        contourf_extend='both',  # 'auto' 'neither' 'min' 'max' 'both'
         col_map='default',
         do_colorbar=False,      # 'all' 'column' 'row' False True
         cbar_ax_lim=(0.1, 0.05, 0.8, 0.02),
@@ -36,15 +36,19 @@ def plot(plot_params):
         cbar_ticklabels=False,
         cbar_label='units',
 
+        # intvl_in=['monthly', '3hr'],#, '3hr'],#, '3hr'],
         intvl_in='monthly',
         intvl_out='jas',
+        # dtype_in_time=['ts', 'inst'],#, 'inst'],# 'inst'],
         dtype_in_time='ts',
+        # dtype_in_vert=[False, 'sigma'],#, 'sigma'],# 'sigma'],
         dtype_in_vert='pressure',
         dtype_out_time='reg.av',
+        # dtype_out_vert=[False, 'vert_int'],#, 'vert_int'],# 'vert_int'],
         dtype_out_vert=False,
         level=False,
+        # yr_range=(1983, 1983),
         yr_range='default',
-
         plot_type='line',
         x_dim='x',
         y_dim='p',
@@ -59,7 +63,8 @@ def plot(plot_params):
         ax_right_label=False,
 
         # Axis limits
-        x_lim=[(-60, 5), (-1, 1), (-20, 20), (-0.2, 0.2)],
+        # x_lim=[(-4, 4), (-2, 2)],
+        x_lim=False,
         x_ticks=False,
         x_ticklabels=False,
         x_label=False,
@@ -70,11 +75,13 @@ def plot(plot_params):
         y_label=False,
 
         lat_lim=(-5, 35),
+        # lat_lim=(-30,30),
         lat_ticks=False,
         lat_ticklabels=False,
         lat_label=False,
 
         lon_lim=(-43, 65),
+        # lon_lim=(-180,180),
         lon_ticks=False,
         lon_ticklabels=False,
         lon_label=False,
@@ -120,57 +127,37 @@ def plot(plot_params):
 
 class MainParams(object):
     """Interface to main routine."""
-    pass
+    def __init__(self):
+        pass
+
+    def prep_data(self):
+        proj = aospy_user.to_proj(self.proj)
+        model = aospy_user.to_model(self.model, proj)
+        run = aospy_user.to_run(self.run, model, proj)
+        var = aospy_user.to_var(self.var)
+        region = aospy_user.to_region(self.region, proj=proj)
+        proj, model, var, region = [aospy_user.to_iterable(obj)
+                                    for obj in (proj, model, var, region)]
+        self.proj = proj
+        self.model = model
+        self.run = run
+        self.var = var
+        self.region = region
 
 
-class PlotParams(object):
-    """Interface to plot routine."""
-    pass
-
-
-class AxParamParser(object):
-    """Container designating that the contents correspond to an Ax."""
-    def __init__(self, params):
-        self.params = params
-        self.n_param = len(params)
-
-
-def main(params):
-    # Instantiate objects and load default/all models, runs, and regions.
-    proj = aospy_user.to_proj(params.proj)
-    model = aospy_user.to_model(params.model, proj)
-    run = aospy_user.to_run(params.run, model, proj)
-    var = aospy_user.to_var(params.var)
-    region = aospy_user.to_region(params.region, proj=proj)
-    proj, model, var, region = [aospy_user.to_iterable(obj)
-                                for obj in (proj, model, var, region)]
-    plot_params = PlotParams()
-    plot_params.proj = proj
-    plot_params.model = model
-    plot_params.run = run
-    plot_params.ens_mem = params.ens_mem
-    plot_params.var = var
-    plot_params.region = region
-    return plot(plot_params)
+def main(main_params):
+    matplotlib.rcParams['font.family'] = 'sans-serif'
+    matplotlib.rcParams['font.sans-serif'] = 'Helvetica'
+    main_params.prep_data()
+    return plot(main_params)
 
 if __name__ == '__main__':
     params = MainParams()
     params.proj = p.aero_3agcm
     params.model = m.am2
-    params.run = ([r.am2_reyoi_cont]*2 +
-                  [{(r.am2_reyoi_p2, r.am2_reyoi_cont): '-'}]*2)
-    # params.model = [m.am2, m.am3, m.hiram, m.hiram_c48]
-    # params.run = [r.am2_reyoi_cont, r.am3_hc, r.hiram_cont, r.hiram_c48_0]
-    # params.run = [r.am2_reyoi_p2, r.am3_hp2k, r.hiram_gtm, r.hiram_c48_0_p2K]
-    # params.run = [{(r.am2_reyoi_p2, r.am2_reyoi_cont): '-'},
-    #               {(r.am3_hp2k, r.am3_hc): '-'},
-    #               {(r.hiram_gtm, r.hiram_cont): '-'},
-    #               {(r.hiram_c48_0_p2K, r.hiram_c48_0): '-'}]
+    params.run = r.am2_reyoi_cont
     params.ens_mem = False
-    params.var = [v.omega, v.moist_static_stab]*2
-    params.region = reg.sahel_north
-
-    matplotlib.rcParams['font.family'] = 'sans-serif'
-    matplotlib.rcParams['font.sans-serif'] = 'Helvetica'
+    params.var = v.mse_horiz_advec
+    params.region = reg.sahel
 
     fig = main(params)
