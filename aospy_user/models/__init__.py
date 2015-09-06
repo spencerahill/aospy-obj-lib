bebda88073686704afcc95035a8353adc5d00e6f
spencerahill <spencerahill@gmail.com>
2015-09-06 14:49:18 -0400

    Update CMIP5 proj; consistent w/ aospy 'gfdl_repo' functionality

--- aospy_user/models/__init__.py
+++ aospy_user/models/__init__.py
@@ -8,7 +8,7 @@ from obs_models import (
 )
 from cmip5_models import (
     bcc_csm1, bnu_esm, cccma_canam4, cccma_cancm4, cccma_canesm2, cmcc_cesm,
-    cmcc_cm, cmcc_cms, cnrc_cms, cnrc_cms_2, cola_cfsv2, csiro_bom_access1_0,
+    cmcc_cm, cmcc_cms, cnrm_cm5, cnrm_cm5_2, cola_cfsv2, csiro_bom_access1_0,
     csiro_bom_access1_3, csiro_qccce_mk3_6_0, fio_esm, ichec_ec_earth, inm_cm4,
     inpe_hadgem2_es, ipsl_cm5a_lr, ipsl_cm5a_mr, ipsl_cm5b_lr,
     lasg_cess_fgoals_g2, lasg_iap_fgoals_g1, lasg_iap_fgoals_s2, miroc4h,
@@ -19,5 +19,5 @@ from cmip5_models import (
     ncar_ccsm4, ncc_noresm1_m, ncc_noresm1_me, ncep_cfsv2_2011,
     nimr_kma_hadgem2_ao, gfdl_cm2_1, gfdl_cm3, gfdl_esm2m, gfdl_esm2g,
     gfdl_hiram_c180, gfdl_hiram_c360, cesm1_bgc, cesm1_cam5, cesm1_fastchem,
-    cesm1_waccm, smhi_ec_earth, unsw_csiro_mk3l_1_2    
+    cesm1_waccm, smhi_ec_earth, unsw_csiro_mk3l_1_2
 )
--- aospy_user/runs/cmip5_runs.py
+++ aospy_user/runs/cmip5_runs.py
@@ -1,7 +1,75 @@
 """aospy.Run objects pertaining to CMIP5 simulations."""
 from aospy import Run
 
-onepctco2 = Run(
+onepctCO2 = Run(
     name='1pctCO2',
     description='Coupled; 1% increase in CO2 per year from PI',
+)
+abrupt4xCO2 = Run(
+    name='abrupt4xCO2',
+    description='Coupled; Instantaneous 4X CO2 increase',
+)
+amip = Run(
+    name='amip',
+    description='Atmosphere only',
+    direc_nc='mon/atmos/Amon/r1i1p1',
+    nc_dir_struc='gfdl_repo',
+    default_yr_range=(1979, 2008),
+)
+amip4K = Run(
+    name='amip4K',
+    description='Atmosphere only',
+    direc_nc='mon/atmos/Amon/r1i1p1',
+    nc_dir_struc='gfdl_repo',
+    default_yr_range=(1979, 2008),
+)
+amip4xCO2 = Run(
+    name='amip4xCO2',
+    description='Atmosphere only',
+    default_yr_range=(1979, 2008),
+)
+amipFuture = Run(
+    name='amipFuture',
+    description='Atmosphere only',
+    default_yr_range=(1979, 2008),
+)
+historical = Run(
+    name='historical',
+    description='Coupled',
+)
+rcp26 = Run(
+    name='rcp26',
+    description='Coupled',
+)
+rcp45 = Run(
+    name='rcp45',
+    description='Coupled',
+)
+rcp60 = Run(
+    name='rcp60',
+    description='Coupled',
+)
+rcp86 = Run(
+    name='rcp86',
+    description='Coupled',
+)
+sstClim = Run(
+    name='sstClim',
+    description='Atmosphere only',
+)
+sstClim4xCO2 = Run(
+    name='sstClim4xCO2',
+    description='Atmosphere only',
+)
+aquaControl = Run(
+    name='aquaControl',
+    description='Atmosphere only',
+)
+aqua4K = Run(
+    name='aqua4K',
+    description='Atmosphere only',
+)
+aqua4xCO2 = Run(
+    name='aqua4xCO2',
+    description='Atmosphere only',
 )
