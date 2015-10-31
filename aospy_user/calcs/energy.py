61eb07698b6a9d5b4508c44145875624b54786ae
spencerahill <spencerahill@gmail.com>
2015-10-30 23:29:21 -0400

    Column energy budget tendency, source, divergence functions

--- aospy_user/calcs/energy.py
+++ aospy_user/calcs/energy.py
@@ -1,8 +1,12 @@
 """Energy budget-related fields"""
+from aospy.constants import grav
 from aospy.utils import int_dp_g
 
+from .tendencies import time_tendency
 from .advection import (horiz_advec, vert_advec, horiz_advec_upwind,
                         vert_advec_upwind, total_advec_upwind)
+from .mass import (column_flux_divg, column_flux_divg_with_adj,
+                   dry_mass_column_budget_residual, budget_residual)
 from .transport import (field_horiz_flux_divg, field_vert_flux_divg,
                         field_total_advec, field_horiz_advec_divg_sum,
                         field_times_horiz_divg)
@@ -11,8 +15,46 @@ from .toa_sfc_fluxes import column_energy
 
 
 def kinetic_energy(u, v):
-    """Kinetic energy, neglecting vertical motion."""
+    """Kinetic energy per unit mass, neglecting vertical motion."""
     return 0.5*(u*u + v*v)
+
+
+def energy(temp, z, q, u, v):
+    return mse(temp, z, q) + kinetic_energy(u, v)
+
+
+def energy_column(temp, z, q, u, v, dp):
+    return int_dp_g(energy(temp, z, q, u, v), dp)
+
+
+def energy_column_tendency(temp, z, q, u, v, dp, freq='1M'):
+    return time_tendency(energy_column(temp, z, q, u, v, dp), freq=freq)
+
+
+energy_column_source = column_energy
+
+
+def energy_column_divg(temp, z, q, u, v, dp, radius):
+    return column_flux_divg(energy(temp, z, q, u, v), u, v, radius, dp)
+
+
+def energy_column_budget_lhs(temp, z, q, u, v, ps, dp, radius, freq='1M'):
+    tendency = energy_column_tendency(temp, z, q, u, v, dp, freq=freq)
+    transport = energy_column_divg(temp, z, q, u, v, dp, radius)
+    return budget_residual(tendency, transport, freq=freq)
+
+
+def energy_column_divg_with_adj(temp, z, q, u, v, ps, dp, radius):
+    return column_flux_divg_with_adj(energy(temp, z, q, u, v), u, v, q,
+                                     ps, radius, dp)
+
+
+def energy_column_divg_with_adj2(temp, z, q, u, v, ps, dp, radius, freq='1M'):
+    lhs = energy_column_budget_lhs(temp, z, q, u, v, ps, dp, radius, freq=freq)
+    dry_mass_resid = dry_mass_column_budget_residual(ps, u, v, q, radius, dp,
+                                                     freq=freq)
+    energy_col = energy_column(temp, z, q, u, v, dp)
+    return lhs - dry_mass_resid * grav.value * energy_col / ps
 
 
 def mse_horiz_flux_divg(temp, hght, sphum, u, v, radius):
@@ -70,7 +112,7 @@ def mse_budget_advec_residual(temp, hght, sphum, ucomp, vcomp, omega,
     """Residual in vertically integrated MSE budget."""
     mse_ = mse(temp, hght, sphum)
     transport = field_total_advec(mse_, ucomp, vcomp, omega, p, radius)
-    trans_vert_int = int_dp_g(transport, dp)[:,np.newaxis,:,:]
+    trans_vert_int = int_dp_g(transport, dp)
     f_net = column_energy(swdn_toa, swup_toa, olr, swup_sfc, swdn_sfc,
                           lwup_sfc, lwdn_sfc, shflx, evap)
     return f_net - trans_vert_int
@@ -83,7 +125,7 @@ def fmse_budget_advec_residual(temp, hght, sphum, ice_wat, ucomp, vcomp, omega,
     """Residual in vertically integrated MSE budget."""
     fmse_ = fmse(temp, hght, sphum, ice_wat)
     transport = field_total_advec(fmse_, ucomp, vcomp, omega, p, radius)
-    trans_vert_int = int_dp_g(transport, dp)[:,np.newaxis,:,:]
+    trans_vert_int = int_dp_g(transport, dp)
     f_net = column_energy(swdn_toa, swup_toa, olr, swup_sfc, swdn_sfc,
                           lwup_sfc, lwdn_sfc, shflx, evap)
     return f_net - trans_vert_int
