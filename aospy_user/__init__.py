b45d910c8a9777da92ae9bce0b9b6f6a006042ff
spencerahill <spencerahill@gmail.com>
2015-10-31 17:17:21 -0400

    Update __init__ to reflect previous refactors

--- aospy_user/__init__.py
+++ aospy_user/__init__.py
@@ -2,13 +2,13 @@
 from aospy import (LAT_STR, LON_STR, PHALF_STR, PFULL_STR, PLEVEL_STR,
                    TIME_STR, TIME_STR_IDEALIZED)
 
-from . import regions
 from . import units
+from . import regions
+from . import finite_diff
+from .finite_diff import FiniteDiff
+from . import sphere_harm
 from . import calcs
 from . import variables
 from . import runs
 from . import models
 from . import projs
-from . import obj_from_name
-from .obj_from_name import (to_proj, to_model, to_run, to_var, to_region,
-                            to_iterable)
