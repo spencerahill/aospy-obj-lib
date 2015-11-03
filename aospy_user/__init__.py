c4f3954ff6956f82cf5174e0e22a3246709ef9af
spencerahill <spencerahill@gmail.com>
2015-11-03 10:25:06 -0500

    Move finite_diff into its own package

--- aospy_user/__init__.py
+++ aospy_user/__init__.py
@@ -1,11 +1,9 @@
-"""aospy_user: Library of user-defined aospy objects."""
+"""My library of aospy objects used for my research."""
 from aospy import (LAT_STR, LON_STR, PHALF_STR, PFULL_STR, PLEVEL_STR,
                    TIME_STR, TIME_STR_IDEALIZED)
 
 from . import units
 from . import regions
-from . import finite_diff
-from .finite_diff import FiniteDiff
 from . import sphere_harm
 from . import calcs
 from . import variables
