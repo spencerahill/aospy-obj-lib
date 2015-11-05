3f1f7d58d3a8d3b6b6920d7460d1afcb90cd7949
spencerahill <spencerahill@gmail.com>
2015-11-05 17:21:25 -0500

    BUG aospy.calcs operators are bad; use their .value attr

--- aospy_user/calcs/thermo.py
+++ aospy_user/calcs/thermo.py
@@ -10,22 +10,22 @@ def dse(temp, hght):
     :param temp: Temperature.  Units: Kelvin.
     :param hght: Geopotential height. Units: meters.
     """
-    return c_p*temp + grav*hght
+    return c_p.value*temp + grav.value*hght
 
 
 def mse(temp, hght, sphum):
     """Moist static energy, in Joules per kilogram."""
-    return dse(temp, hght) + L_v*sphum
+    return dse(temp, hght) + L_v.value*sphum
 
 
 def fmse(temp, hght, sphum, ice_wat):
     """Frozen moist static energy, in Joules per kilogram."""
-    return mse(temp, hght, sphum) - L_f*ice_wat
+    return mse(temp, hght, sphum) - L_f.value*ice_wat
 
 
 def pot_temp(temp, p, p0=1000.):
     """Potential temperature.  Units: Kelvin."""
-    return temp*(p0/p)**kappa
+    return temp*(p0/p)**kappa.value
 
 
 def virt_pot_temp(temp, p, sphum, liq_wat, p0=1000.):
@@ -37,7 +37,7 @@ def virt_pot_temp(temp, p, sphum, liq_wat, p0=1000.):
 
 def equiv_pot_temp(temp, p, sphum, p0=1000.):
     """Equivalent potential temperature."""
-    return pot_temp(temp + L_v*sphum/c_p, p, p0=p0)
+    return pot_temp(temp + L_v.value*sphum/c_p, p, p0=p0)
 
 
 def mixing_ratio_from_specific_mass(mass):
@@ -52,20 +52,22 @@ def specific_mass_dry_air(q_v, q_l, q_s):
 def specific_gas_constant_moist_air(q_v, q_l, q_s):
     """Specific gas constant of moist air with the given water masses."""
     q_a = specific_mass_dry_air(q_v, q_l, q_s)
-    return q_a*R_a + q_v*R_v
+    return q_a*R_a.value + q_v*R_v.value
 
 
 def heat_capacity_moist_air_constant_volume(q_v, q_l, q_s):
     """Heat capacity at constant volume of moist air."""
     q_a = specific_mass_dry_air(q_v, q_l, q_s)
-    return c_va*q_a + c_vv*q_v + c_vl*q_l + c_vs*q_s
+    return c_va.value*q_a + c_vv.value*q_v + c_vl.value*q_l + c_vs.value*q_s
 
 
 def specific_entropy_dry_air(T, p):
     """Specific entropy of dry air.  From Romps."""
-    return c_p*np.log(T/T_trip) - R_d*np.log(p / p_trip)
+    return (c_p.value*np.log(T/T_trip.value) -
+            R_d.value*np.log(p / p_trip.value))
 
 
 def specific_entropy_water_vapor(T, p):
     """Specific entropy of water vapor.  From Romps."""
-    return c_p*np.log(T/T_trip) - R_d*np.log(p / p_trip)
+    return (c_p.value*np.log(T/T_trip.value) -
+            R_d.value*np.log(p / p_trip.value))
