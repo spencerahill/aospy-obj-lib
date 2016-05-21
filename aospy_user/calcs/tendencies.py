58d21220bd94cc9b2d2cd02473a69fdb0ca62a7d
spencerahill <spencerahill@gmail.com>
2016-05-21 01:15:14 -0400

    MAINT remove obsolete workarounds for xarray bugs since resolved

--- aospy_user/calcs/tendencies.py
+++ aospy_user/calcs/tendencies.py
@@ -1,5 +1,4 @@
 """Calculations involved in mass and energy budgets."""
-from aospy.utils import coord_to_new_dataarray
 from infinite_diff import CenDeriv
 import numpy as np
 
@@ -8,7 +7,7 @@ from .. import TIME_STR
 
 def first_to_last_vals_dur(arr, freq='1M'):
     """Time elapsed between 1st and last values in each given time period."""
-    time = coord_to_new_dataarray(arr, TIME_STR)
+    time = arr[TIME_STR]
     first = time.resample(freq, TIME_STR, how='first')
     last = time.resample(freq, TIME_STR, how='last')
     delta_time = last - first
@@ -24,15 +23,18 @@ def time_tendency_first_to_last(arr, freq='1M'):
     return (last - first) / first_to_last_vals_dur(arr, freq)
 
 
+def cen_diff_time(arr):
+    """Time centered differencing"""
+    time = arr[TIME_STR].copy()
+    # Force time units to be seconds.
+    time = (time - np.datetime64(0, 's')) / np.timedelta64(1, 's')
+    return CenDeriv(arr, TIME_STR, coord=time, fill_edge=True).deriv()
+
+
 def time_tendency_each_timestep(arr):
     """Time tendency of the given field at each timestep.
 
     Compute via centered differencing at interior timesteps, one-sided
     differencing at endpoints.
     """
-    def cen_diff_time(arr):
-        time = arr[TIME_STR].copy()
-        # Force time units to be seconds.
-        time = (time - np.datetime64(0, 's')) / np.timedelta64(1, 's')
-        return CenDeriv(arr, TIME_STR, coord=time, fill_edge=True).deriv()
     return arr.groupby('time.year').apply(cen_diff_time)
